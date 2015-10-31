35f9821cf36f825257c2862af87bece2749c111f
Matt Weldon <matt.weldon@gmail.com>
2015-10-31 01:19:24 +0000

    add support for calling raw sql with parameters

--- lib/exql.ex
+++ lib/exql.ex
@@ -6,4 +6,16 @@ defmodule Exql do
     |> Exql.Transformer.transform
   end
 
+  def execute(sql, params) do
+    Exql.Runner.connect!
+    |> Tds.Connection.query(sql, create_params(params))
+    |> Exql.Transformer.transform
+  end
+
+  defp create_params(params) do
+    Enum.map(params, fn({k, v}) ->
+      %Tds.Parameter{name: "@#{Atom.to_string(k)}", value: v}
+    end)
+  end
+
 end
--- test/exql_test.exs
+++ test/exql_test.exs
@@ -4,7 +4,11 @@ defmodule ExqlTest do
 
   test "runs raw sql and returns sane results" do
     results = Exql.execute("select * from people")
-    IO.inspect results
     assert Enum.count(results) == 2
+  end
+
+  test "runes raw sql with params" do
+    results = Exql.execute("select * from people where id = @user_id", [user_id: 1])
+    assert Enum.count(results) == 1
   end
 end
