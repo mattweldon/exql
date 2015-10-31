a8a107b05a4a9099e361c00fad832af17eaf798d
Matt Weldon <matt.weldon@gmail.com>
2015-10-31 09:13:49 +0000

    move calls out to tds into Exql.Runner module

--- lib/exql/query.ex
+++ lib/exql/query.ex
@@ -29,12 +29,14 @@ defmodule Exql.Query do
     %{query | criteria: query.criteria ++ [criteria], params: query.params ++ params} |> build_sql
   end
 
+  @doc """
+  Connects to the database, executes the given query and returns the results.
+  """
   def execute(query) do
     Exql.Runner.connect!
-    |> Tds.Connection.query(query.sql, query.params |> create_params)
+    |> Exql.Runner.send_query(query)
     |> Exql.Transformer.transform
   end
-
 
   @doc """
   Builds the final sql statement.
@@ -48,10 +50,5 @@ defmodule Exql.Query do
         %{query | sql: "select #{query.scope} from #{query.table} where #{flat_criteria}"}
     end
   end
-
-  defp create_params(params) do
-    Enum.map(params, fn({k, v}) ->
-      %Tds.Parameter{name: "@#{Atom.to_string(k)}", value: v}
-    end)
-  end
+  
 end
--- lib/exql/runner.ex
+++ lib/exql/runner.ex
@@ -4,10 +4,20 @@ defmodule Exql.Runner do
     {:ok, pid} = connect
     pid
   end
-  
+
   def connect do
     Application.get_env(:exql, :connection)
     |> Tds.Connection.start_link
+  end
+
+  def send_query(conn, query) do
+    Tds.Connection.query(conn, query.sql, query.params |> create_params)
+  end
+
+  defp create_params(params) do
+    Enum.map(params, fn({k, v}) ->
+      %Tds.Parameter{name: "@#{Atom.to_string(k)}", value: v}
+    end)
   end
 
 end
